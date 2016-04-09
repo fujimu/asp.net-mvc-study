148bcd0b62bde2a7e4f6b92410ecb460034c8406
TYSfujimura <fujimura.tomohiro@toyoko-sys.co.jp>
2016-04-09 12:53:52 +0900

    タイトルでの検索機能の追加 ジャンルでの検索機能の追加

--- HelloWorldApp/HelloWorldApp/Models/Movie.cs
+++ HelloWorldApp/HelloWorldApp/Models/Movie.cs
@@ -7,11 +7,15 @@ namespace HelloWorldApp.Models
     public class Movie
     {
         public int ID { get; set; }
+        [Display(Name = "タイトル")]
         public string Title { get; set; }
         [Display(Name ="リリース日")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        [Display(Name = "ジャンル")]
         public string Genre { get; set; }
+        [Display(Name ="価格")]
+        [DataType(DataType.Currency)]
         public decimal Prioe { get; set; }
     }
 
